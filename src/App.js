235a8880b3ad71be6e894780a9ef85aad3020e49
sossw1 <sossw1@gmail.com>
2021-08-12 19:40:46 -0400

    Base auth component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
 function App() {
   return (
     <div className="App">
-
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
