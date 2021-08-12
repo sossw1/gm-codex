d0e7b01c94aac9641dff52da92112def799ba3b2
sossw1 <sossw1@gmail.com>
2021-08-12 19:40:08 -0400

    configure Amplify

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import 'semantic-ui-css/semantic.min.css';
 
+//Amplify
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
