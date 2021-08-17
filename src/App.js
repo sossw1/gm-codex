645e28b0005a18e143065ee082391d6ebdcd2b90
sossw1 <sossw1@gmail.com>
2021-08-17 19:02:42 -0400

    Removed boilerplate auth ui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
 function App() {
   return (
     <div className="App">
-      <AmplifySignOut />
+      
     </div>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
