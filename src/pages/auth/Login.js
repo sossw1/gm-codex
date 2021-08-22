6ecb93a0719983f741da3e5e6d1edf3460d027d7
sossw1 <sossw1@gmail.com>
2021-08-22 18:44:21 -0400

    username and password in local state

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,19 +1,33 @@
-import './Login.css';
+import { useState } from 'react';
 
 function Login() {
+  const [username, setUsername] = useState('');
+  function handleUsernameChange(event) {
+    setUsername(event.target.value);
+  }
+
+  const [password, setPassword] = useState('');
+  function handlePasswordChange(event) {
+    setPassword(event.target.value);
+  }
+
   return(
     <form className='ui form icon input'>
       <div className='ui segment'>
         <div className='field'>
           <div className='ui left icon input'>
             <i className='user circle icon'></i>
-            <input type='text' name='username' placeholder='username'></input>
+            <input
+              type='text' name='username' placeholder='username'
+              onChange={handleUsernameChange}
+            ></input>
           </div>
         </div>
         <div className='field'>
           <div className='ui left icon input'>
             <i className='lock icon'></i>
-            <input type='password' name='password' placeholder='password'></input>
+            <input type='password' name='password' placeholder='password'
+            onChange={handlePasswordChange}></input>
           </div>
         </div>
         <div className='ui fluid large grey submit button'>
