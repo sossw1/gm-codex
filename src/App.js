ce042a36f421939cd12402bd76843090f4fc383d
sossw1 <sossw1@gmail.com>
2021-09-08 15:07:43 -0400

    base sign in form

--- src/App.js
+++ src/App.js
@@ -4,26 +4,35 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
   const initialFormState = {
-    username: '', password: '', email: '', authCode: '', formType: 'signUp'
+    username: '', password: '', email: '', authCode: '', formType: 'signIn'
   }
 
   const [formState, setFormState] = useState(initialFormState);
   function onChange(e) {
     e.persist();
-    setFormState(() => ({...formState, [e.target.name]: e.target.value}))
+    setFormState(() => ({ ...formState, [e.target.name]: e.target.value }))
   }
 
-  const {formType} = formState;
+  const { formType } = formState;
 
   return (
     <div className="App">
       {
         formType === 'signUp' && (
           <div>
-            <input name='username' onchange={onChange} placeholder='username'/><br/>
-            <input name='email' onchange={onChange} placeholder='email'/><br/>
-            <input name='password' type='password' onchange={onChange} placeholder='password'/><br/>
+            <input name='username' onchange={onChange} placeholder='username' /><br />
+            <input name='email' onchange={onChange} placeholder='email' /><br />
+            <input name='password' type='password' onchange={onChange} placeholder='password' /><br />
             <button>Sign Up</button>
+          </div>
+        )
+      }
+      {
+        formType === 'signIn' && (
+          <div>
+            <input name='username' onchange={onChange} placeholder='username' /><br/>
+            <input name='password' type='password' onchange={onChange} placeholder='password' /><br/>
+            <button>Sign In</button>
           </div>
         )
       }
