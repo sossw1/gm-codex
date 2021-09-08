9460dfa11ef8b950efbda5a3ed32716714928433
sossw1 <sossw1@gmail.com>
2021-09-08 15:05:19 -0400

    conditionally render base signup form

--- src/App.js
+++ src/App.js
@@ -6,15 +6,27 @@ function App() {
   const initialFormState = {
     username: '', password: '', email: '', authCode: '', formType: 'signUp'
   }
+
   const [formState, setFormState] = useState(initialFormState);
   function onChange(e) {
     e.persist();
     setFormState(() => ({...formState, [e.target.name]: e.target.value}))
   }
 
+  const {formType} = formState;
+
   return (
     <div className="App">
-      
+      {
+        formType === 'signUp' && (
+          <div>
+            <input name='username' onchange={onChange} placeholder='username'/><br/>
+            <input name='email' onchange={onChange} placeholder='email'/><br/>
+            <input name='password' type='password' onchange={onChange} placeholder='password'/><br/>
+            <button>Sign Up</button>
+          </div>
+        )
+      }
     </div>
   );
 }
