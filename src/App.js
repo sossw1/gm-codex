23c6c35f8f28329e3669e707241489c61beb3b06
sossw1 <sossw1@gmail.com>
2021-09-08 14:50:51 -0400

    Initial form state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Login from './pages/auth/Login';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const initialFormState = {
+    username: '', password: '', email: '', authCode: '', formType: 'signUp'
+  }
+  const [formState, setFormState] = useState(initialFormState);
+
   return (
     <div className="App">
-      <Login />
+      
     </div>
   );
 }
