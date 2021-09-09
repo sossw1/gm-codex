7acf89be4906f126a4909543c21814ac2e9dfc95
sossw1 <sossw1@gmail.com>
2021-09-09 14:05:12 -0400

    Sign up form component

---
+++ src/pages/auth/SignUp.js
@@ -0,0 +1,37 @@
+function SignUp(props) {
+  return(
+    <form className='ui form icon input' onSubmit={props.signUp}>
+      <div className='ui segment'>
+        <div className='field'>
+          <div className='ui left icon input'>
+            <i className='user circle icon'></i>
+            <input
+              type='text' name='username' placeholder='username'
+              onChange={props.onChange}
+            ></input>
+          </div>
+        </div>
+        <div className='field'>
+          <div className='ui left icon input'>
+            <i className='envelope outline icon'></i>
+            <input
+              type='text' name='email' placeholder='email'
+              onChange={props.onChange}
+            ></input>
+          </div>
+        </div>
+        <div className='field'>
+          <div className='ui left icon input'>
+            <i className='lock icon'></i>
+            <input type='password' name='password' placeholder='password'
+              onChange={props.onChange}></input>
+          </div>
+        </div>
+        <input type='submit' value='Sign Up'
+          className='ui fluid large grey submit button'></input>
+      </div>
+    </form>
+  )
+}
+
+export default SignUp;
