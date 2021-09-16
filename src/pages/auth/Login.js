c3b1bb60eaf1aeeb258597f32d8597e668f9a240
sossw1 <sossw1@gmail.com>
2021-09-15 20:50:06 -0400

    two column grid

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -2,29 +2,38 @@ import '../../App.css';
 
 function Login(props) {
   return (
-    <form className='ui form icon input' onSubmit={props.signIn}>
-      <div className='ui segment'>
-        <div className='field'>
-          <div className='ui left icon input'>
-            <i className='user circle icon'></i>
-            <input
-              type='text' name='username' placeholder='username'
-              onChange={props.onChange}
-            ></input>
-          </div>
-        </div>
-        <div className='field'>
-          <div className='ui left icon input'>
-            <i className='lock icon'></i>
-            <input type='password' name='password' placeholder='password'
-              onChange={props.onChange}></input>
-          </div>
-        </div>
-        <input type='submit' value='Login'
-          className='ui fluid large grey submit button'></input>
-        <p>Don't have an account? <span className='link' onClick={() => props.authLink('signUp')}>Sign up</span></p>
+    <div className='ui two column padded grid'>
+      <div className='column'>
+        <div></div>
+          <form className='ui form icon input' onSubmit={props.signIn}>
+            <div className='ui segment'>
+              <div className='field'>
+                <div className='ui left icon input'>
+                  <i className='user circle icon'></i>
+                  <input
+                    type='text' name='username' placeholder='username'
+                    onChange={props.onChange}
+                  ></input>
+                </div>
+              </div>
+              <div className='field'>
+                <div className='ui left icon input'>
+                  <i className='lock icon'></i>
+                  <input type='password' name='password' placeholder='password'
+                    onChange={props.onChange}></input>
+                </div>
+              </div>
+              <input type='submit' value='Login'
+                className='ui fluid large grey submit button'></input>
+              <p>Don't have an account? <span className='link' onClick={() => props.authLink('signUp')}>Sign up</span></p>
+            </div>
+          </form>
+
       </div>
-    </form>
+      <div className='column'>
+        
+      </div>
+    </div>
   );
 }
 
