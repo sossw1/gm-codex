e3901ec2be4b4080e41d57a2c2a7f86b2d7e536d
sossw1 <sossw1@gmail.com>
2021-10-22 18:08:23 -0400

    home page route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Home from './pages/Home'
+// React Router
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Switch>
+          <Route path='/' component={Home} />
+        </Switch>
+      </Router>
     </div>
   );
 }
